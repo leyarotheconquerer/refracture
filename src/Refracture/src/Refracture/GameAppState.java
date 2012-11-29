872a2b8d621f751fda56b05aa60f805ac57aa29a
Hazen <HazenJohnson@letu.edu>
2012-11-28 20:38:36 -0600

    Made a very little progress on the class structure.

--- src/Refracture/src/Refracture/GameAppState.java
+++ src/Refracture/src/Refracture/GameAppState.java
@@ -4,6 +4,8 @@ import com.jme3.app.Application;
 import com.jme3.app.SimpleApplication;
 import com.jme3.app.state.AbstractAppState;
 import com.jme3.app.state.AppStateManager;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -22,15 +24,38 @@ public class GameAppState extends AbstractAppState {
     /** A local instance of the overall game */
     private SimpleApplication app;
     
+    //Node Section
+    
+    /** The map parent node. */
+    Node map;
+        /** Terrain that is influenced by earthquakes. */
+        Node destructibleTerrain;
+        /** Terrain that is not influenced by earthquakes. */
+        Node indestructibleTerrain;
+            /** The physical map itself */
+            Geometry terrain;
+    
     @Override
     public void initialize(AppStateManager stateManager, Application app)
-    {
+    {   
         super.initialize(stateManager, app);
         
         //Store the passed in application
         this.app = (SimpleApplication) app;
         
-        logger.log(Level.INFO, "GameAppState initialize not yet implemented.");
+        //Load the nodes
+        
+        //Load the map structure
+        logger.log(Level.INFO, "Creating map nodes");
+        map = new Node("map");
+        //Add sub-nodes
+        destructibleTerrain = new Node("destructibleTerrain");
+        map.attachChild(destructibleTerrain);
+        
+        indestructibleTerrain = new Node("indestructibleTerrain");
+        map.attachChild(indestructibleTerrain);
+        
+        logger.log(Level.INFO, "GameAppState initialize not fully implemented.");
     }
     
     @Override
