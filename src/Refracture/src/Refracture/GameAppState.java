9477ee3b3ba0602e133e40a2854ce0bb1fb2805b
Hazen <HazenJohnson@letu.edu>
2012-11-29 14:49:14 -0600

    Began to make basic code for a test building.
    Filled out the scene graph some more.
    
    When run, this should now display a building floating in the air...
    because I can't figure out bounding volumes... sigh.

--- src/Refracture/src/Refracture/GameAppState.java
+++ src/Refracture/src/Refracture/GameAppState.java
@@ -4,12 +4,21 @@ import com.jme3.app.Application;
 import com.jme3.app.SimpleApplication;
 import com.jme3.app.state.AbstractAppState;
 import com.jme3.app.state.AppStateManager;
+import com.jme3.bullet.collision.shapes.CollisionShape;
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.bullet.util.CollisionShapeFactory;
+import com.jme3.collision.CollisionResult;
+import com.jme3.collision.CollisionResults;
 import com.jme3.light.AmbientLight;
 import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Ray;
 import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+import com.jme3.scene.debug.WireBox;
 import com.jme3.terrain.geomipmap.TerrainQuad;
 import com.jme3.terrain.heightmap.AbstractHeightMap;
 import com.jme3.terrain.heightmap.ImageBasedHeightMap;
@@ -35,7 +44,13 @@ public class GameAppState extends AbstractAppState {
     //Node Section
     
     /** The map parent node. */
-    Node map;
+    Node mapRoot;
+    
+    /** The building parent node. */
+    Node buildingRoot;
+    
+    /** The earthquake parent node. */
+    Node earthquakeRoot;
     
     @Override
     public void initialize(AppStateManager stateManager, Application app)
@@ -45,7 +60,9 @@ public class GameAppState extends AbstractAppState {
         //Store the passed in application
         this.app = (SimpleApplication) app;
         
-        //Load the nodes
+        ////////////////////////////////////////////////////////////////////////
+        //Load all the nodes////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////
         
         /**
          * A pointer to the current position in the scene graph.
@@ -56,21 +73,46 @@ public class GameAppState extends AbstractAppState {
         //Load the lights
         makeSun();
         
-        //Load the map structure
+        //Load the map structure////////////////////////////////////////////////
         logger.log(Level.INFO, "Creating map nodes");
-        map = new Node("map");
-        iterator.attachChild(map);
+        mapRoot = new Node("mapRoot");
+        iterator.attachChild(mapRoot);
         
-        //Fill in the children of "map"
-        iterator = (Node)iterator.getChild("map");
+        //Fill in the children of "mapRoot"
+        iterator = (Node)iterator.getChild("mapRoot");
         iterator.attachChild(new Node("destructibleTerrain"));
         iterator.attachChild(new Node("indestructibleTerrain"));
         
         //Fill in the children of "indestructibleTerrain"
         iterator = (Node)iterator.getChild("indestructibleTerrain");
-        iterator.attachChild(makeTerrain());
-        iterator.getChild("Terrain").setLocalTranslation(
+        iterator.attachChild(makeTerrain("terrain"));
+        iterator.getChild("terrain").setLocalTranslation(
                 new Vector3f(0,-30f,0));
+        
+        //Reset the iterator
+        iterator = this.app.getRootNode();
+        
+        //Load the building structure///////////////////////////////////////////
+        logger.log(Level.INFO, "Creating building nodes");
+        buildingRoot = new Node("buildingRoot");
+        iterator.attachChild(buildingRoot);
+        
+        //Fill in the children of "buildingRoot"
+        iterator = (Node)iterator.getChild("buildingRoot");
+        iterator.attachChild(new Node("defenses"));
+        iterator.attachChild(new Node("other"));
+        
+        //Fill in the children of "other"
+        iterator = (Node) iterator.getChild("other");
+        iterator.attachChild(makeTestBuilding(new Vector3f(0,0,0), "testBuilding"));
+        
+        //Reset the iterator
+        iterator = this.app.getRootNode();
+        
+        //Load the earthquake structure/////////////////////////////////////////
+        logger.log(Level.INFO, "Creating earthquake nodes");
+        earthquakeRoot = new Node("earthquakeRoot");
+        iterator.attachChild(earthquakeRoot);
         
         logger.log(Level.INFO, "GameAppState initialize not fully implemented.");
     }
@@ -104,10 +146,31 @@ public class GameAppState extends AbstractAppState {
     //jMonkey uses the component system and is thus defined by what it consists
     //of rather than what it is.
     
+    //General Definitions///////////////////////////////////////////////////////
+    
+    /**
+     * Defines a directional light to light the entire scene.
+     */
+    private void makeSun()
+    {
+        DirectionalLight sun = new DirectionalLight();
+        sun.setColor(ColorRGBA.White);
+        sun.setDirection(new Vector3f(-1f, -1f, -1f).normalizeLocal());
+        sun.setName("Sun");
+        this.app.getRootNode().addLight(sun);
+        
+        AmbientLight ambient = new AmbientLight();
+        ambient.setColor(ColorRGBA.White);
+        ambient.setName("Ambient");
+        this.app.getRootNode().addLight(ambient);
+    }
+    
+     //Map Definitions//////////////////////////////////////////////////////////
+    
     /**
      * Defines the map terrain.
      */
-    private Node makeTerrain()
+    private Node makeTerrain(String name)
     {
         TerrainQuad terrain;
         
@@ -138,8 +201,13 @@ public class GameAppState extends AbstractAppState {
             mat.setTexture("Tex3", grass2);
             
             //Create the terrain object
-            terrain = new TerrainQuad("Terrain", 64, 257, heightMap.getHeightMap());
+            terrain = new TerrainQuad(name, 64, 257, heightMap.getHeightMap());
             terrain.setMaterial(mat);
+            
+            terrain.updateModelBound();
+            /*
+            WireBox geom = new WireBox();
+            geom.fromBoundingBox(terrain.getWorldBound());*/
             
             return terrain;
         } catch (Exception ex) {
@@ -150,19 +218,44 @@ public class GameAppState extends AbstractAppState {
         return null;
     }
     
+    //Building Definitions//////////////////////////////////////////////////////
+    
     /**
-     * Defines a directional light to light the entire scene.
+     * Loads a test building and places it.
      */
-    private void makeSun()
+    private Spatial makeTestBuilding(Vector3f localTranslation, String name)
     {
-        DirectionalLight sun = new DirectionalLight();
-        sun.setColor(ColorRGBA.White);
-        sun.setDirection(new Vector3f(-1f, -1f, -1f).normalizeLocal());
-        sun.setName("Sun");
-        this.app.getRootNode().addLight(sun);
+        //Load the model
+        Spatial building = this.app.getAssetManager().loadModel("Models/testbuilding.j3o");
+        building.setName(name);
+        building.setLocalTranslation(localTranslation);
         
-        AmbientLight ambient = new AmbientLight();
-        ambient.setColor(ColorRGBA.White);
-        ambient.setName("Ambient");
+        //Load the texture
+        Material mat = new Material(this.app.getAssetManager(),
+                "Common/MatDefs/Light/Lighting.j3md");
+        mat.setTexture("DiffuseMap",
+                this.app.getAssetManager().loadTexture("Textures/testbuilding.png"));
+        building.setMaterial(mat);
+        
+        //Move the building onto the terrain
+        logger.log(Level.INFO, "Finding ground collision of {0}", name);
+        Ray ray = new Ray(localTranslation,
+                new Vector3f(0,-1f,0).normalizeLocal());
+        CollisionResults results = new CollisionResults();
+        this.app.getRootNode().collideWith(ray, results);
+        
+        for (int i = 0; i < results.size(); i++) {
+            String object = results.getCollision(i).getGeometry().getName();
+            Vector3f point = results.getCollision(i).getContactPoint();
+            logger.log(Level.INFO, "Collision at {0} with {1}",
+                    new Object[]{point.toString(), object});
+        }
+        
+        CollisionResult closest = results.getClosestCollision();
+        logger.log(Level.INFO, "Moving {0} to {1}",
+                new Object[]{name, closest.getContactPoint().toString()});
+        building.setLocalTranslation(closest.getContactPoint());
+        
+        return building;
     }
 }
