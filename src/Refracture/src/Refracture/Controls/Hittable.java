b6e0114da2de3a166dea64f7fde916bb966fb73f
Lizaim <dewaepolk@letu.edu>
2012-12-02 16:47:02 -0600

    Changed the, added the hitAction function, which gains information from the earthquake that hit it.

--- src/Refracture/src/Refracture/Controls/Hittable.java
+++ src/Refracture/src/Refracture/Controls/Hittable.java
@@ -8,12 +8,16 @@ import com.jme3.scene.control.Control;
  *
  * @author DeWae
  */
-public abstract class Hittable extends AbstractControl {
-    
-boolean hit; // The boolean to see if the objects are hit.
+public abstract class Hittable extends AbstractControl {    
 
 String earthquakeX; // Identifies the earthquake that hit the object at hand.
 
 String rayX; // Identifies what Ray that actually hit the object at hand.
 
+   // This Function is invoked when something hittable is struck
+	public void hitAction(){
+	// Get name of Earthquake, set to earthquakeX
+	// Get the name of the ray, set to rayX
+	// Pass strength from ray to extended classes
+        }
 }
